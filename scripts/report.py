d8b74a9c107b02d67a9575dfbd9ac2607de046a9
Kirill Izotov <enykeev@stackstorm.com>
2016-12-21 00:30:28 +0700

    Add milestones to the report

--- scripts/report.py
+++ scripts/report.py
@@ -29,17 +29,32 @@ if __name__ == '__main__':
 
     for section in config:
         defined_labels = [label['name'] for label in section.get('labels')]
+        defined_milestones = [milestone['title'] for milestones in section.get('milestones')]
 
         labels_unknown = {}
+        milestones_unknown = {}
 
         for repo_name in section.get('repositories'):
-            for label in g.get_repo(repo_name).get_labels():
+            repo = g.get_repo(repo_name)
+
+            for label in repo.get_labels():
                 if label.name not in defined_labels:
                     if repo_name not in labels_unknown:
                         labels_unknown[repo_name] = []
 
                     labels_unknown[repo_name].append(label.name)
 
+            for milestone in repo.get_milestones():
+                if milestone.title not in defined_milestones:
+                    if repo_name not in milestones_unknown:
+                        milestones_unknown[repo_name] = []
+                    
+                    milestones_unknown[repo_name].append(milestone.title)
+
         if labels_unknown:
             print('# Labels that are not in spec:')
             print(yaml.safe_dump(labels_unknown, default_flow_style=False))
+        
+        if milestones_unknown:
+            print('# Milestones that are not in spec:')
+            print(yaml.safe_dump(milestones_unknown, default_flow_style=False))
